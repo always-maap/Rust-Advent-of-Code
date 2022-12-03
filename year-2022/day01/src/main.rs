5fa9f7954f5281a331d9c12a80ef7cf8d63d55a6
tur1ng <mohammadali.ap.2000@gmail.com>
2022-12-03 09:40:19 +0330

    2022: day01 test

--- year-2022/day01/src/main.rs
+++ year-2022/day01/src/main.rs
@@ -39,3 +39,35 @@ fn part2(input: &str) -> u32 {
     println!("{:?}", each_elf);
     each_elf.iter().take(3).sum()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn part1_works() {
+        let result = part1(INPUT);
+        assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = part2(INPUT);
+        assert_eq!(result, 45000);
+    }
+}
