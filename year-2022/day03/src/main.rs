aa019a26039c446482bf192a0203d45985b81b9c
tur1ng <mohammadali.ap.2000@gmail.com>
2022-12-05 02:24:21 +0330

    2022: day03 part2

--- year-2022/day03/src/main.rs
+++ year-2022/day03/src/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, fs};
 
+use itertools::Itertools;
+
 fn main() {
     let input = fs::read_to_string("input.txt")
         .expect("Error reading the file")
@@ -8,6 +10,9 @@ fn main() {
 
     let p1 = part1(&input);
     println!("Part 1: {}", p1);
+
+    let p2 = part2(&input);
+    println!("Part 2: {}", p2);
 }
 
 fn part1(input: &str) -> usize {
@@ -29,6 +34,31 @@ fn part1(input: &str) -> usize {
         .sum::<usize>()
 }
 
+fn part2(input: &str) -> usize {
+    let letters = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(idx, c)| (c, idx + 1))
+        .collect::<HashMap<char, usize>>();
+
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let mut chunk = chunk.into_iter();
+            let a = chunk.next().unwrap();
+            let b = chunk.next().unwrap();
+            let c = chunk.next().unwrap();
+            let common_char = a
+                .chars()
+                .find(|a_char| b.contains(*a_char) && c.contains(*a_char))
+                .unwrap();
+            letters.get(&common_char).unwrap()
+        })
+        .sum::<usize>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,5 +74,11 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     fn part1_works() {
         let result = part1(INPUT);
         assert_eq!(result, 157);
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = part2(INPUT);
+        assert_eq!(result, 70);
     }
 }
