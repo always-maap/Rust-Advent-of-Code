015bf6fb7929e9985de003bbe95aed00bd9af8d9
tur1ng <mohammadali.ap.2000@gmail.com>
2022-12-06 01:22:28 +0330

    2022: day04 part1

--- year-2022/day04/src/main.rs
+++ year-2022/day04/src/main.rs
@@ -1,4 +1,6 @@
-use std::fs;
+use std::{fs, ops::RangeInclusive};
+
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("input.txt")
@@ -13,8 +15,37 @@ fn main() {
     println!("Part 2: {}", p2);
 }
 
+trait InclusiveRangeExt {
+    fn contains_range(&self, other: &Self) -> bool;
+}
+
+impl<T> InclusiveRangeExt for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+}
+
 fn part1(input: &str) -> usize {
-    0
+    input
+        .lines()
+        .map(|line| {
+            line.split(",")
+                .map(|range| {
+                    range
+                        .split("-")
+                        .map(|n| n.parse().unwrap())
+                        .collect_tuple::<(u32, u32)>()
+                        .map(|(start, end)| start..=end)
+                        .unwrap()
+                })
+                .collect_tuple::<(_, _)>()
+                .unwrap()
+        })
+        .filter(|(a, b)| a.contains_range(b) || b.contains_range(a))
+        .count()
 }
 
 fn part2(input: &str) -> usize {
@@ -25,12 +56,17 @@ fn part2(input: &str) -> usize {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
     #[test]
     fn part1_works() {
         let result = part1(INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 2);
     }
 
     #[test]
