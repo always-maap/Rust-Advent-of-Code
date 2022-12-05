ddad255d89a114475b48eab8109fc418c219a1d1
tur1ng <mohammadali.ap.2000@gmail.com>
2022-12-06 01:31:15 +0330

    2022: day05 scaffold

---
+++ year-2022/day05/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let input = fs::read_to_string("input.txt")
+        .expect("Error reading the file")
+        .trim()
+        .to_string();
+
+    let p1 = part1(&input);
+    println!("Part 1: {}", p1);
+
+    let p2 = part2(&input);
+    println!("Part 2: {}", p2);
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part1_works() {
+        let result = part1(INPUT);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = part2(INPUT);
+        assert_eq!(result, 0);
+    }
+}
